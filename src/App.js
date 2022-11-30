446cbb5778dce55955d34e35b5f1c56d0e11a464
vito <vitowidigdo@gmail.com>
2022-11-30 16:31:07 +0700

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [open, setOpen] = useState(false);
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery("");
+      setOpen(!open);
+    }
+  };
+
+  const image = weather?.weather;
+  //   console.log({ weather });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && open && (
+        <div className="city">
+          <h2 className="city-name">
+            <span className="">{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+            <div className="img-open" onClick={() => setOpen(!open)}>
+              <img
+                className="close-img"
+                src={require("./img/close.png")}
+                alt={`a href="https://www.flaticon.com/free-icons/close" title="close icons">Close icons created by ariefstudio - Flaticon</a>`}
+                style={{
+                  width: "10px",
+                  height: "10px",
+                }}
+              />
+            </div>
+          </h2>
+
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${image[0].icon}@2x.png`}
+              alt={image[0].description}
+            />
+
+            <p></p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
